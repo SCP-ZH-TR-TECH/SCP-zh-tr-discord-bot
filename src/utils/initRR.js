3124b515c6dcf8dbcb4ba06f04b5246f33c1ef2c
Joch2520 <20432565+Joch2520@users.noreply.github.com>
2020-02-15 15:42:02 +0800

    added reactionrole and initting msg for reactionrole

---
+++ src/utils/initRR.js
@@ -0,0 +1,22 @@
+const {roleMap} = require('./parser.js');
+
+module.exports = async (bot) => {
+  for (var rolechan in roleMap) {
+    if (roleMap[rolechan].__info.msg) {
+      var msgcontent = [`${roleMap[rolechan].__info.content}\n`];
+      var msg = await bot.channels.get(roleMap[rolechan].__info.chan).fetchMessage(roleMap[rolechan].__info.msg)
+      for (var role in roleMap[rolechan]) {
+        if (role!="__info") {
+          if (roleMap[rolechan][role].reactname) {
+            msgcontent.push(`<:${roleMap[rolechan][role].reactname}:${roleMap[rolechan][role].react}>: \`${roleMap[rolechan][role].rolename}\``);
+            msg.react(msg.guild.emojis.get(roleMap[rolechan][role].react));
+          } else {
+            msgcontent.push(`${roleMap[rolechan][role].react}: \`${roleMap[rolechan][role].rolename}\``);
+            msg.react(roleMap[rolechan][role].react);
+          }
+        }
+      }
+      msg.edit(msgcontent.join("\n\n"))
+    }
+  }
+}
