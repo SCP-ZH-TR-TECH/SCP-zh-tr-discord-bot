6709521173a87bef9173f5dd6fb0894efe5ea23e
Joch2520 <20432565+Joch2520@users.noreply.github.com>
2020-03-27 18:53:24 +0800

    edited welcome msg, djs dependency and channel mapping

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,5 +1,5 @@
-var greetmsg = `歡迎您加入SCP-ZH繁中分部的聊天室！新成員 :)\n我是駐守此地的Mosaic.aic！為了之後相處愉快的每一天\n
-請認真的讀過<#577140915010011156> 並前往<#645229994657710102>按照興趣領取身分組\n有疑問及遇到困難時請跟名字是鮮黃色跟橘子色的管理員求助喔\n最後請幫Mo簡單寫一下自我介紹，讓大家認識你哦。\n\nHello, we have some guides in <#626440362268950528> for international friends who don't speak Chinese. You can also get an international role for yourself there.`
+var greetmsg = `歡迎您加入SCP-ZH繁中分部的聊天室！新成員 :)\n我是駐守此地的Mos.aic！為了之後相處愉快的每一天\n\n
+請確認您已年滿15歲，並認真的讀過<#577140915010011156> 並前往<#645229994657710102>按照興趣領取身分組\n有疑問及遇到困難時請跟名字是鮮黃色跟橘子色的管理員求助喔\n最後請幫Mo簡單寫一下自我介紹，讓大家認識你哦。\n\nHello, we have some guides in <#626440362268950528> for international friends who don't speak Chinese. You can also get an international role for yourself there.\n\n**Please note you have to confirm that you are __over 15 years old in age.__**`
 var intro = `自我介紹：\n\n常用的稱呼？\n有沒有wikidot帳號/ID是什麼？\n學經歷與性別？（可保密）\n對SCP基金會了解多深？\n怎麼入坑的？\n
 目前有興趣的事：聊天/翻譯外文作品/進行原創作品/繪製同人圖畫/其他？`
 
